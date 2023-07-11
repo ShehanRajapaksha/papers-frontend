9880ca901ff6aa75592d6e75ef7e3a82a4bc20d9
thanura mendis <thanuramendis02@gmail.com>
2023-07-11 12:52:50 +0530

    footer added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Footer from './components/Footer';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-tailwind/react';
 
@@ -11,6 +12,7 @@ root.render(
     <ThemeProvider>
       <App />
     </ThemeProvider>
+    <Footer />
 
   </React.StrictMode>
 );
