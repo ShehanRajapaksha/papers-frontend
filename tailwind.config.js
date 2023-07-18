f5542947b35de3dc098aeff54488060373705444
Shehan Rajapaksha <126370980+ShehanRajapaksha@users.noreply.github.com>
2023-07-18 14:28:12 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+     dark:'#3D3D3D',
+     stone:'#a8a29e',
+     back:'#556B2F',
+    
+
+    }
+    },
   },
   plugins: [],
 });
