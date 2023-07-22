22097cfc880baa9f5d56925326049654ce1d8e56
Shehan Rajapaksha <126370980+ShehanRajapaksha@users.noreply.github.com>
2023-07-22 22:45:28 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,97 @@ import {
   IconButton,
   Input,
   Collapse,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem
 } from "@material-tailwind/react";
+
+function GradeMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const closeMenu = () => setIsMenuOpen(false);
+ 
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+      <MenuHandler>
+      
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-2 px-4 font-normal hover:shadow-md hover:bg-gray-100 hover:font-light  rounded-xl transition duration-300 text-base cursor-pointer"
+      >
+    
+          Grades
+     
+         
+      
+      </Typography> 
+      </MenuHandler>
+        <MenuList className="p-1">
+            {Array.from({ length: 13 }, (_, index) => (
+              <MenuItem
+                key={`Grade ${index + 1}`}
+                onClick={closeMenu}
+                className={`flex items-center gap-2 rounded `}
+              >
+                <Typography as="span" variant="small" className="font-normal">
+                  Grade {index + 1}
+                </Typography>
+              </MenuItem>
+            ))}
+        </MenuList>
+    </Menu>
+  );
+}
+
+
+ 
+
 
 export default function Header() {
   const [openNav, setOpenNav] = React.useState(false);
+  const [isMenuOpenMobile, setIsMenuOpenMobile] = React.useState(false);
+  const closeMenuMobile = () => setIsMenuOpenMobile(false);
+
+  
+  const RenderGradeMenu = () => {
+    
+
+    if (window.innerWidth >= 720) {
+      return <GradeMenu />;
+    }
+    return( 
+     <ul> 
+    <Typography
+      as="li"
+      variant="small"
+      color="blue-gray"
+      className="p-2 px-4 font-normal  hover:shadow-md hover:bg-gray-100 transition rounded-xl duration-300 text-base"
+      onClick={() => setIsMenuOpenMobile(!isMenuOpenMobile)}
+    >
+
+        Grades
+      <ul>
+      {isMenuOpenMobile && (
+        <div className="p-2 " >
+          {Array.from({ length: 13 }, (_, index) => (
+            <li><div
+              key={`Grade ${index + 1}`}
+              onClick={closeMenuMobile}
+              className='flex items-center gap-2 rounded p-2 cursor-pointer w-full  border-b-2 font-light'
+            >
+              <span className=" px-3  hover:font-light ">Grade {index + 1}</span>
+            </div>
+            </li>
+          ))}
+        </div>
+      )}
+      </ul>
+  </Typography>
+  </ul>);
+  };
+
 
   React.useEffect(() => {
     window.addEventListener(
@@ -17,25 +104,20 @@ export default function Header() {
       () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
+ 
 
   const navList = (
     <ul className=" mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 md:pr-4 ">
+      {/* Grade Menu  */}
+    
+      
+      <RenderGradeMenu/>
       
       <Typography
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-2 font-normal hover:shadow-md hover:bg-gray-100 hover:font-light  transition duration-300"
-      >
-        <a href="" className="flex items-center mr-2">
-          Pages
-        </a>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-2 font-normal  hover:shadow-md hover:bg-gray-100 transition duration-300"
+        className="p-2 px-4 font-normal  hover:shadow-md hover:bg-gray-100 transition rounded-xl duration-300 text-base"
       >
         <a href="" className="flex items-center">
           Account
@@ -45,7 +127,7 @@ export default function Header() {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-2 font-normal  hover:shadow-md hover:bg-gray-100 transition duration-300"
+        className="p-2 px-4 rounded-xl font-normal  hover:shadow-md hover:bg-gray-100 transition duration-300 text-base"
       >
         <a href="" className="flex items-center">
           Blocks
@@ -55,7 +137,7 @@ export default function Header() {
         as="li"
         variant="small"
         color="blue-gray"
-        className="p-2 font-normal  hover:shadow-md hover:bg-gray-100 transition duration-300"
+        className="p-2 px-4 rounded-xl font-normal  hover:shadow-md hover:bg-gray-100 transition duration-300 text-base"
       >
         <a href="" className="flex items-center">
           Docs
@@ -66,7 +148,7 @@ export default function Header() {
 
   return (
     <>
-      <Navbar className="sticky top z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-2">
+      <Navbar className="sticky top z-10 h-max max-w-full rounded-none py-2 px-4 lg:px-8 lg:py-4">
         <div className={`flex flex-col lg:flex-row  justify-between text-blue-gray-900  ${window.innerWidth < 1024 ? 'items-end' : 'items-center'}`}>
           <div className="flex items-center my-2">
             <div className={`lg:border-r border-blue-gray-700  ${window.innerWidth < 1024 ? '' : 'pr-4 mr-4'}`}>
@@ -79,8 +161,25 @@ export default function Header() {
                 Material Tailwind
               </Typography>
             </div>
-            <div className="hidden lg:block">{navList}</div>
+            
           </div>
+          <div className="relative py-2 px-3 flex flex-row w-full gap-2 md:w-max md:flex-row md:items-center" >
+          <div className="hidden lg:block">{navList}</div>
+          </div>
+
+          {/* <div className="relative py-2 px-3 flex flex-row w-full gap-2 md:w-max md:flex-row md:items-center" >
+            <Input
+              type="search"
+              label="Type here..."
+              className="pr-20"
+              containerProps={{
+                className: "w-full md:w-auto",
+              }}
+            />
+            <Button size="sm" className="!md:absolute md:right-1 md:top-2 rounded">
+              Search
+            </Button>
+          </div> */}
           
           <div className="lg:hidden absolute top-5 left-4">
             <IconButton
@@ -124,6 +223,7 @@ export default function Header() {
         </div>
         <Collapse open={openNav}>
           {navList}
+         
         </Collapse>
       </Navbar>
     </>
